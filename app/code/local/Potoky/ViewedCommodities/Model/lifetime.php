a854ecb90bb46a50b423288976199fa5e28f41c2
AlexWorking <workmail2500@gmail.com>
2018-05-30 02:55:02 +0300

    home 0254300518 (final)

--- app/code/local/Potoky/ViewedCommodities/Model/lifetime.php
+++ app/code/local/Potoky/ViewedCommodities/Model/lifetime.php
@@ -7,26 +7,26 @@ class Potoky_ViewedCommodities_Model_Lifetime extends Mage_Core_Model_Config_Dat
      * and saves the configuration in case of success.
      *
      * @return Mage_Core_Model_Abstract
-     * @throws Mage_Core_Exception $e
+     * @throws Mage_Core_Exception
      */
     public function save()
     {
         $lifetime = $this->getValue();
         $sessCookLifetime = (int) Mage::getStoreConfig(Mage_Core_Model_Cookie::XML_PATH_COOKIE_LIFETIME);
-        if (!preg_match('#^[\s]*$#', $lifetime)) {
-            if (filter_var($lifetime, FILTER_VALIDATE_INT, array(
-                "options" => array(
-                    "min_range" => 0,
-                ))) === false) {
+        if (filter_var($lifetime, FILTER_VALIDATE_INT, array(
+            "options" => array(
+                "min_range" => 120,
+                "max_range" => 7776000
+            ))) === false) {
 
-                Mage::throwException('The value of the field either should be empty or be equal zero or an integer.');
-            } elseif ($lifetime > $sessCookLifetime) {
-                Mage::getSingleton('adminhtml/session')
-                    ->addNotice(
-                        'Please mind that the  lifetime of the JS block will not last longer then the session cookie lifetime
-                         which for now is configured to ' . $sessCookLifetime . ' sec. It is also strictly dependant on other sessional lifetimes.'
-                    );
-            }
+            Mage::throwException('The value of the field should be an integer in range from 120 to 7776000.');
+        }
+        elseif ($lifetime > $sessCookLifetime) {
+            Mage::getSingleton('adminhtml/session')
+                ->addNotice(
+                    'Please mind that the  lifetime of the JS block will not last longer then the session cookie lifetime
+                     which for now is configured to ' . $sessCookLifetime . ' sec. It is also strictly dependant on other sessional lifetimes.'
+                );
         }
         return parent::save();
     }
