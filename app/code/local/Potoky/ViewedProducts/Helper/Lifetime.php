6e9469575092d15031ca9e545608d283b40b204b
AlexWorking <workmail2500@gmail.com>
2018-06-18 12:46:59 +0000

    Fifth Commit. Lifetime.php helper obtained back its static property. (It is still used more than one time).

--- app/code/local/Potoky/ViewedProducts/Helper/Lifetime.php
+++ app/code/local/Potoky/ViewedProducts/Helper/Lifetime.php
@@ -3,17 +3,24 @@
 class Potoky_ViewedProducts_Helper_Lifetime extends Mage_Core_Helper_Abstract
 {
     /**
+     * Lifetime of JS Block
+     *
+     * @var int
+     */
+    private static $lifetime;
+
+    /**
      * Retrieves the lifetime of the JS Block.
      *
-     * @return int $lifetime | null
+     * @return int self::$lifetime
      */
     public function getLifetime()
     {
-        if (Mage::getStoreConfig('catalog/js_viewed_products/allow_jsblock')) {
+        if (!isset(self::$lifetime) && Mage::getStoreConfig('catalog/js_viewed_products/allow_jsblock')) {
             $lifetime = Mage::getStoreConfig('catalog/js_viewed_products/viewedproducts_lifetime');
-            return $lifetime;
+            self::$lifetime = $lifetime;
         }
 
-        return null;
+        return self::$lifetime;
     }
 }
