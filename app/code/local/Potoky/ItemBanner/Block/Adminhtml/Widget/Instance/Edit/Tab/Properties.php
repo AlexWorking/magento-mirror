915dffbf7df8a3096ce0fafa4441ac52c156d3ea
AlexWorking <workmail2500@gmail.com>
2018-08-02 07:22:57 -0600

    work 172202082018

---
+++ app/code/local/Potoky/ItemBanner/Block/Adminhtml/Widget/Instance/Edit/Tab/Properties.php
@@ -0,0 +1,42 @@
+<?php
+
+class Potoky_ItemBanner_Block_Adminhtml_Widget_Instance_Edit_Tab_Properties extends Mage_Widget_Block_Adminhtml_Widget_Instance_Edit_Tab_Properties
+{
+    /*protected function _prepareForm()
+    {
+        $parent = parent::_prepareForm();
+        $widgetInstance = $this->getWidgetInstance();
+        $form = $parent->getForm();
+        $fieldset = $form->addFieldSet('banner_related_properties',
+            array('legend' => Mage::helper('itembanner')->__('Banner Related Properties'))
+        );
+        $fieldset->addField('banner_image', 'file', array(
+            'name' => 'image',
+            'label' => Mage::helper('widget')->__('Banner Image'),
+            'title' => Mage::helper('widget')->__('Banner Image'),
+            'class' => '',
+            'required' => true,
+            'note' => Mage::helper('widget')->__('Image that will cover the whole banner')
+        ));
+        $layoutBlock = $this->getLayout()
+            ->createBlock('widget/adminhtml_widget_instance_edit_tab_main_layout')
+            ->setWidgetInstance($widgetInstance);
+        $form->getElement('banner_image')->setRenderer($layoutBlock);
+
+        return $parent;
+    }*/
+
+    /**
+     * Prepare block children and data.
+     * Set widget type and widget parameters if available
+     *
+     * @return Mage_Widget_Block_Adminhtml_Widget_Instance_Edit_Tab_Properties
+     */
+    protected function _preparelayout()
+    {
+
+        $this->setWidgetType($this->getWidgetInstance()->getType())
+            ->setWidgetValues($this->getWidgetInstance()->getWidgetParameters());
+        return parent::_prepareLayout();
+    }
+}
--- app/code/local/Potoky/ItemBanner/Block/Banner.php
+++ app/code/local/Potoky/ItemBanner/Block/Banner.php
@@ -3,7 +3,7 @@
 class Potoky_ItemBanner_Block_Banner extends Mage_Core_Block_Abstract implements Mage_Widget_Block_Interface
 {
     protected function _toHtml() {
-        $html = '<h1>MY WIDGET</h1>';
+        $html = '<img src="' . Mage::getBaseUrl("media") . 'itembanner/Lioness-Roar-PNG-Pic.png" alt="HHHHH">';
         return $html;
     }
 }
