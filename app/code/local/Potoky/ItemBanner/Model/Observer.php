25b7f8b777e3f5849180e68803fede710d2634a6
AlexWorking <workmail2500@gmail.com>
2018-09-18 20:17:56 +0300

    home 201718092018

--- app/code/local/Potoky/ItemBanner/Model/Observer.php
+++ app/code/local/Potoky/ItemBanner/Model/Observer.php
@@ -12,35 +12,8 @@ class Potoky_ItemBanner_Model_Observer
     {
         $widgetInstance = $observer->getEvent()->getObject();
 
-        return $this;
-    }
-
-
-    public function addSearchHandles($widgetInstance)
-    {
-        if($widgetInstance->getType() != "itembanner/banner") {
-
-            return $this;
-        }
-        $pageGroups = $widgetInstance->getData('page_groups');
-        foreach ($pageGroups as &$pageGroup) {
-            if (in_array('catalog_category_layered', $pageGroup['layout_handle_updates']) ||
-                in_array('catalog_category_default', $pageGroup['layout_handle_updates'])) {
-                $pageGroup['layout_handle_updates'][] = 'catalogsearch_result_index';
-                $pageGroup['layout_handle_updates'][] = 'catalogsearch_advanced_result';
-            }
-        }
-        unset($pageGroup);
-        $widgetInstance->setData('page_groups', $pageGroups);
-
-        if (!$widgetInstance->getId()) {
-            $itemBannerInfo = Mage::getModel('itembanner/bannerinfo')->getCollection()
-                ->addFieldToFilter('is_active',array('eq'=>true))
-                ->selectFirstItem();
-            if ($itemBannerInfo->getId()) {
-                $itemBannerInfo->setData('is_active', false);
-                $itemBannerInfo->save();
-            }
+        if($widgetInstance->getType() == "itembanner/banner") {
+            $this->addSearchHandles($widgetInstance)->deactivate($widgetInstance);
         }
 
         return $this;
@@ -52,7 +25,43 @@ class Potoky_ItemBanner_Model_Observer
      * @param Varien_Event_Observer $observer
      * @return $this
      */
-    private function disactivate($widgetInstance) {
+    public function registerInfoToDb($observer)
+    {
+        $widgetInstance = $observer->getEvent()->getObject();
 
+        $itemBannerInfo = Mage::getModel('itembanner/bannerinfo');
+        $itemBannerInfo ->setData('instance_id', $widgetInstance->getId());
+        $itemBannerInfo->save();
+        return $this;
+    }
+
+    private function addSearchHandles($widgetInstance)
+    {
+        $pageGroups = $widgetInstance->getData('page_groups');
+        foreach ($pageGroups as &$pageGroup) {
+            if (in_array('catalog_category_layered', $pageGroup['layout_handle_updates']) ||
+                in_array('catalog_category_default', $pageGroup['layout_handle_updates'])) {
+                $pageGroup['layout_handle_updates'][] = 'catalogsearch_result_index';
+                $pageGroup['layout_handle_updates'][] = 'catalogsearch_advanced_result';
+            }
+        }
+        unset($pageGroup);
+        $widgetInstance->setData('page_groups', $pageGroups);
+
+        return $this;
+    }
+
+    private function deactivate($widgetInstance)
+    {
+        $itemBannerInfo = Mage::getModel('itembanner/bannerinfo')->getCollection()
+            ->addFieldToFilter('is_active',array('eq'=>true))
+            ->selectFirstItem();
+
+        if ($widgetInstance->getId() != $itemBannerInfo->getId()) {
+            $itemBannerInfo->setData('is_active', false);
+            $itemBannerInfo->save();
+        }
+
+        return $this;
     }
 }
